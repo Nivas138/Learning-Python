8590c3d8cbf976448a5422a1f3bb46dd6e56306b
Nivas138 <38051335+Nivas138@users.noreply.github.com>
2018-07-08 14:51:10 +0530

    Prime Numbers within a Limit

---
+++ prime_number_loop.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  8 13:22:07 2018
+
+@author: Nivas
+"""
+def prime(number):
+    flag=1
+    if number is 0 or number is 1:
+        flag=0
+    elif number == 2:
+        flag=1
+    elif number%2 == 0:
+        flag=flag+1
+    else:
+        for j in range(3,number,2):
+            if number%j == 0:
+               flag=flag+1
+               break
+            else:
+               continue
+    return flag
+print('Prime Number within Limit')
+num=int(input("Enter a limit: "))
+for i in range(0,num):
+    flag=1
+    flags=prime(i)
+    if flags is 1:
+        print (i)
